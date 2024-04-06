f493ad7f1ca4c118e54c0d21c8043d128c55ff74
Daniel Armando Valdez Reyes <77825594+Danval-003@users.noreply.github.com>
2024-04-06 01:46:51 -0600

    update server configs

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ return p, r, m
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    print(app.config)
+    return 'Hello World!'+ str(os.getenv('NEO4J_URI'))
 
 
 if __name__ == '__main__':
